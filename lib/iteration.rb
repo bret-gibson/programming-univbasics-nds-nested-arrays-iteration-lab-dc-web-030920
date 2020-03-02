c4750da70a3ecc1d2e55da26bd9dff4742083b7b
Bret Gibson <bret.gibson@gmail.com>
2020-03-02 20:22:30 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -59,7 +59,7 @@ def total_even_pairs(src)
       element_index += 1
     end
     if even
-      sum += src[row_index][0] + src[row_index][1]
+      sum += (src[row_index][0] + src[row_index][1])
     end
     row_index += 1
   end
